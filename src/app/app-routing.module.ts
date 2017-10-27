b77e9767a3977cb0e86c8ba987577f0e3822fe90
Edward Alexis OrtIZ Agudelo <alexixsortizz@gmail.com>
2017-10-26 19:45:42 -0500

    User list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
+
 const routes: Routes = [
-    { path: '', redirectTo: '/appComponent', pathMatch: 'full' },
-    { path: 'appComponent', component: AppComponent }
+    { path: '', redirectTo: '/userComponent', pathMatch: 'full' },
+    { path: 'appComponent', component: AppComponent },
+    { path: 'userComponent', component: UserComponent },
 ];
 
 @NgModule({
